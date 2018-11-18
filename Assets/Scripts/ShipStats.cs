96ee91630dc0fdc672ea0ca2188f7f90da620958
BuildTools <unconfigured@null.spigotmc.org>
2018-11-18 14:13:32 -0500

    Enemies have Health
    Corrected position and enemies have health
    Cannonballs should no longer go off course

---
+++ Assets/Scripts/ShipStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipStats : MonoBehaviour {
+
+    private int health;
+
+	// Use this for initialization
+	void Start () {
+        health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(health < 1)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("inside!");
+
+        if(other.tag == "MiniCannonball")
+        {
+            health -= 50;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Cannonball")
+        {
+            health -= 100;
+            Destroy(other.gameObject);
+        }
+    }
+
+}
