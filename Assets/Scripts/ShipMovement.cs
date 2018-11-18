e29ce106465f5c6b879bc9fb73a596b6db8fd7b1
BuildTools <unconfigured@null.spigotmc.org>
2018-11-18 14:27:59 -0500

    DockHealth
    DockHealth Hooked up fro p2

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipMovement : MonoBehaviour {
-
+    public GameObject gameManager;
 	// Use this for initialization
 	void Start () {
-		
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.right * .02f);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.tag == "Dock")
+        {
+            Debug.Log("hit!");
+            Destroy(this.gameObject);
+            gameManager.GetComponent<GameStats>().p2DockHealth -= 20;
+        }
+    }
+
 }
